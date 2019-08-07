9822b492545195a5625160c5c211e454c6996b97
andreas <andreas.luethi@gmx.net>
2019-08-07 16:39:23 +0200

    Some refactoring, write directly a file for canplayer (could also be done with: "asc2log -v -I PermanentLogging.ASC -O PermanentLogging.log" !!)

--- tcanlogplayer.py
+++ tcanlogplayer.py
@@ -28,7 +28,7 @@ def validLine(line):
     except OSError:
         return False
 
-## 8672.21581 1  1930       Rx D 8  10  12  22 182 255 191 255 224
+## "8672.21581 1  1930       Rx D 8  10  12  22 182 255 191 255 224"
 def toCanFrame(line):
     parts = (" ".join(line.split()).split())
     ts = float(parts[0])
@@ -73,29 +73,33 @@ canIds = {}
 nodeIds = {}
 lastTime = 0 
 print("start sending to device {} ...".format(interface))
-with open(filepath) as fp:
-    for cnt, line in enumerate(fp):
-        if cnt < skip_rows:
-            continue
-        if validLine(line):
-            ts, canId, data, nodeId = toCanFrame(line)
-            if cnt == skip_rows:
-                lastTime = ts
-            assert ts > 0.0, "Wrong time increment";
-            if len(filterCanIds) == 0 or canId in filterCanIds:
-                statistics(canIds, canId)
-                statistics(nodeIds, nodeId)
-                
-                dt = ts - lastTime
-                time.sleep(dt)
-                time_now = datetime.now().strftime("%H:%M:%S.%f")
-                print("send at {}: canId={:04} data={}".format(time_now, canId, data.hex()))
-                send(canId, data)
+with open("candump.log", "w") as logfile:
+    with open(filepath) as fp:
+        for cnt, line in enumerate(fp):
+            if cnt < skip_rows:
+                continue
+            if validLine(line):
+                ts, canId, data, nodeId = toCanFrame(line)
+                if cnt == skip_rows:
+                    lastTime = ts
+                assert ts > 0.0, "Wrong time increment";
+                if len(filterCanIds) == 0 or canId in filterCanIds:
+                    statistics(canIds, canId)
+                    statistics(nodeIds, nodeId)
 
-                #sched.enterabs(startTime + ts, 1, lambda x, y: send(x, y), (canId, data,))
-                #sched.run()
-                lastTime = ts
+                    dt = ts - lastTime
+                    time.sleep(dt)
+                    time_now = datetime.now().strftime("%H:%M:%S.%f")
+                    print("send at {}: canId={:04} data={}".format(time_now, canId, data.hex().upper()))
 
+                    # write directly a file for canplayer
+                    # "1563281268.048045) vcan0 78A#0A0C343602490000"
+                    logfile.write("({:f}) vcan0 {:X}#{}\n".format(startTime + ts, canId, data.hex().upper()))
+                    send(canId, data)
+
+                    #sched.enterabs(startTime + ts, 1, lambda x, y: send(x, y), (canId, data,))
+                    #sched.run()
+                    lastTime = ts
 
 print("canId statistics")
 print(sorted(canIds.items(), key=lambda kv: kv[0], reverse=True))
